045595516be1f5f027651901991c13f2655341d3
mvl22 <martin@lucas-smith.co.uk>
2022-09-22 23:53:29 +0100

    Migrate fields definition from database to JSON-backed templatisation in sync with https://github.com/camunigeography/reviewable-assessments/commit/2a9b07d498042c5b51a5d01e81afef01ba58bb5e

--- ethicsAssessments.php
+++ ethicsAssessments.php
@@ -24,51 +24,6 @@ class ethicsAssessments extends reviewableAssessments
 	}
 	
 	
-	# Database structure
-	public function databaseStructureSpecificFields ()
-	{
-		# Return the SQL
-		return $sql = "
-			  /* Domain-specific fields */
-			  
-			  `funder` varchar(255) DEFAULT NULL COMMENT 'Funder',
-			  `title` varchar(255) DEFAULT NULL COMMENT 'Project title',
-			  `topic` text COMMENT 'Summary of topic',
-			  `guidance` enum('','Yes','No') DEFAULT NULL COMMENT 'Read guidance',
-			  `methods` text COMMENT 'Data collection methods',
-			  `conflicts` enum('','Yes','No') DEFAULT NULL COMMENT 'Conflicts of interest',
-			  `conflictsDetails` text COMMENT 'Conflicts of interest - details',
-			  `socialMedia` enum('','Yes','No') DEFAULT NULL COMMENT 'Social media',
-			  `socialMediaDetails` text COMMENT 'Social media - details',
-			  `harm` enum('','Yes','No') DEFAULT NULL COMMENT 'Harmful/intrusive',
-			  `harmDetails` text COMMENT 'Harmful/intrusive - details',
-			  `gatekeeper` enum('','Yes','No') DEFAULT NULL COMMENT 'Gatekeeper',
-			  `gatekeeperDetails` text COMMENT 'Gatekeeper - details',
-			  `organisations` enum('','Yes','No') DEFAULT NULL COMMENT 'Organisations',
-			  `organisationsDetails` text COMMENT 'Organisations - details',
-			  `vulnerable` enum('','Yes','No') DEFAULT NULL COMMENT 'Vulnerable participants',
-			  `vulnerableDetails` text COMMENT 'Vulnerable participants - details',
-			  `sensitiveTopics` enum('','Yes','No') DEFAULT NULL COMMENT 'Sensitive topics',
-			  `sensitiveTopicsDetails` text COMMENT 'Sensitive topics - details',
-			  `humanParticipants` enum('','Yes','No') DEFAULT NULL COMMENT 'Human participants',
-			  `humanParticipantsDetails` text COMMENT 'Human participants - details',
-			  `humanData` enum('','Yes','No') DEFAULT NULL COMMENT 'Data from humans',
-			  `humanDataDetails` text COMMENT 'Data from humans - details',
-			  `covert` enum('','Yes','No') DEFAULT NULL COMMENT 'Covert research',
-			  `covertDetails` text COMMENT 'Covert research - details',
-			  `inducements` enum('','Yes','No') DEFAULT NULL COMMENT 'Inducements',
-			  `inducementsDetails` text COMMENT 'Inducements - details',
-			  `assistants` enum('','Yes','No') DEFAULT NULL COMMENT 'Assistants',
-			  `assistantsDetails` text COMMENT 'Assistants - details',
-			  `distress` ENUM('','Yes','No') DEFAULT NULL COMMENT 'Distress',
-			  `distressDetails` TEXT DEFAULT NULL COMMENT 'Distress - details',
-			  `abroad` enum('','Yes','No') DEFAULT NULL COMMENT 'Abroad',
-			  `abroadDetails` text COMMENT 'Abroad - details',
-			  `dataProtection` text COMMENT 'Data protection',
-			  `stage2InfoRequired` int(1) DEFAULT NULL COMMENT 'Stage 2 information required',
-		";
-	}
-	
 	
 	# Define private data fields
 	public function privateDataFields ()
@@ -95,133 +50,133 @@ class ethicsAssessments extends reviewableAssessments
 			table.questionnaire td select + br + textarea {margin-left: 50px;}
 		</style>
 		
-		<table class="graybox questionnaire">
+		<table class="graybox questionnaire">' . "
 			<tbody>
 				<tr>
 					<td>5. Who is funding your research (e.g. self-funded, ESRC, NERC)?</td>
-					<td>{funder}</td>
+					<td>{funder|varchar(255)|Funder}</td>
 				</tr>
 				<tr>
 					<td>6. Title of Dissertation/Research Project</td>
-					<td>{title}</td>
+					<td>{title|varchar(255)|Project title}</td>
 				</tr>
 				<tr>
 					<td>7. Brief summary of research topic (approx 100 words)</td>
-					<td>{topic}</td>
+					<td>{topic|mediumtext|Summary of topic}</td>
 				</tr>
 				<tr>
 					<td>8. Have you read and understood the &quot;Research ethics: Guiding principles&quot; and &quot;project ethics&quot; sections on the intranet?</td>
-					<td>{guidance}</td>
+					<td>{guidance|enum('','Yes','No')|Read guidance}</td>
 				</tr>
 				<tr>
 					<td>9. List all data collection methods that you intend to use (e.g. interviews, water sampling, archival work)</td>
-					<td>{methods}</td>
+					<td>{methods|mediumtext|Data collection methods}</td>
 				</tr>
 				<tr>
 					<td>10. Do you have any personal or professional conflict of interest that is relevant to the research project? If YES, please describe the actions taken or planned to manage this conflict of interest.</td>
 					<td>
-						{conflicts}<br />
-						{conflictsDetails}
+						{conflicts|enum('','Yes','No')|Conflicts of interest}<br />
+						{conflictsDetails|mediumtext|Conflicts of interest - details}
 					</td>
 				</tr>
 				<tr>
 					<td>11. Does your research involve the use of social media (in terms of accessing and/or disseminating data)? If YES, please explain how you will manage the ethical implications and ensure informed consent.</td>
 					<td>
-						{socialMedia}<br />
-						{socialMediaDetails}
+						{socialMedia|enum('','Yes','No')|Social media}<br />
+						{socialMediaDetails|mediumtext|Social media - details}
 					</td>
 				</tr>
 				<tr>
 					<td>12. Will any of the proposed research be harmful or intrusive to (a) people (e.g. using blood or tissue samples either self-collected or from archives/storage, physical or emotional pain), (b) the environment (e.g. damage to paths, vegetation) and/or (c) animals (e.g. risk to animal welfare)? If YES to one or more of these, how will you address this?</td>
 					<td>
-						{harm}<br />
-						{harmDetails}
+						{harm|enum('','Yes','No')|Harmful/intrusive}<br />
+						{harmDetails|mediumtext|Harmful/intrusive - details}
 					</td>
 				</tr>
 				<tr>
 					<td>13. Will your research need the support of a gatekeeper (e.g. head-teacher, NGO, self-help group, government official) for you to access people/place(s)? If YES, describe how you intend to negotiate access to your research participants/place(s) through this gatekeeper.</td>
 					<td>
-						{gatekeeper}<br />
-						{gatekeeperDetails}
+						{gatekeeper|enum('','Yes','No')|Gatekeeper}<br />
+						{gatekeeperDetails|mediumtext|Gatekeeper - details}
 					</td>
 				</tr>
 				<tr>
 					<td>14. Will you have a formal or informal association with any organisations during your fieldwork (e.g. voluntary work placement, connection to a local organisation/NGO/school, recruitment through the NHS*)? If YES, how have you sought permission to conduct research from those involved?</td>
 					<td>
-						{organisations}<br />
-						{organisationsDetails}
+						{organisations|enum('','Yes','No')|Organisations}<br />
+						{organisationsDetails|mediumtext|Organisations - details}
 					</td>
 				</tr>
 				<tr>
 					<td>15. Will the study involve participants who are vulnerable (e.g. children, homeless, refugees, elderly people)? If YES, describe any special consideration you will give (e.g. DBS for work with under-16s, issues of inclusion, ensuring consent is fully comprehended).</td>
 					<td>
-						{vulnerable}<br />
-						{vulnerableDetails}
+						{vulnerable|enum('','Yes','No')|Vulnerable participants}<br />
+						{vulnerableDetails|mediumtext|Vulnerable participants - details}
 					</td>
 				</tr>
 				<tr>
 					<td>16. Will the study involve discussion of sensitive topics (e.g. sexual activity, drug use, violence, abuse)? If YES, describe how you will handle these topics in order to mitigate psychological stress or harm to participants.</td>
 					<td>
-						{sensitiveTopics}<br />
-						{sensitiveTopicsDetails}
+						{sensitiveTopics|enum('','Yes','No')|Sensitive topics}<br />
+						{sensitiveTopicsDetails|mediumtext|Sensitive topics - details}
 					</td>
 				</tr>
 				<tr>
 					<td>17. Does your research involve human participants? If YES, describe how you will ensure informed consent (please include precise details of the information you will provide to participants to secure their informed consent).</td>
 					<td>
-						{humanParticipants}<br />
-						{humanParticipantsDetails}
+						{humanParticipants|enum('','Yes','No')|Human participants}<br />
+						{humanParticipantsDetails|mediumtext|Human participants - details}
 					</td>
 				</tr>
 				<tr>
 					<td>18. Does your research involve data from humans (dead or alive)? If YES, please describe what steps will you take to ensure the confidentiality of your respondents (whether or not the research will be published).</td>
 					<td>
-						{humanData}<br />
-						{humanDataDetails}
+						{humanData|enum('','Yes','No')|Data from humans}<br />
+						{humanDataDetails|mediumtext|Data from humans - details}
 					</td>
 				</tr>
 				<tr>
 					<td>19. Will it be necessary for participants to take part in the study without their knowledge and consent at the time (e.g. covert observation of people in non-public places)? If YES, how will you mitigate the potential negative consequences and ethical concerns of covert research?</td>
 					<td>
-						{covert}<br />
-						{covertDetails}
+						{covert|enum('','Yes','No')|Covert research}<br />
+						{covertDetails|mediumtext|Covert research - details}
 					</td>
 				</tr>
 				<tr>
 					<td>20. Will financial inducements (other than reasonable expenses and compensation for time) be offered to participants? If YES, how will you ensure this is handled to ensure integrity in your research?</td>
 					<td>
-						{inducements}<br />
-						{inducementsDetails}
+						{inducements|enum('','Yes','No')|Inducements}<br />
+						{inducementsDetails|mediumtext|Inducements - details}
 					</td>
 				</tr>
 				<tr>
 					<td>21. Will the research be conducted by/alongside anyone other than yourself (e.g. translator, hiring enumerators, teachers)? If YES, please describe how will you train them to ensure ethical compliance.</td>
 					<td>
-						{assistants}<br />
-						{assistantsDetails}
+						{assistants|enum('','Yes','No')|Assistants}<br />
+						{assistantsDetails|mediumtext|Assistants - details}
 					</td>
 				</tr>
 				<tr>
 					<td>22. Could your research cause psychological and/or emotional distress to you, the researcher (e.g. due to the topic, group of respondents, methods, location, duration)? If YES, please provide details of the strategies you will employ to limit the potential harm to yourself.</td>
 					<td>
-						{distress}<br />
-						{distressDetails}
+						{distress|enum('','Yes','No')|Distress}<br />
+						{distressDetails|mediumtext|Distress - details}
 					</td>
 				</tr>
 				<tr>
 					<td>23. Do you plan to undertake research outside the UK? If YES, describe any existing links with the country and explain how you intend to manage local cultural and political sensitivities. Please show how you intend to comply with any legal requirements for conducting research in that country.</td>
 					<td>
-						{abroad}<br />
-						{abroadDetails}
+						{abroad|enum('','Yes','No')|Abroad}<br />
+						{abroadDetails|mediumtext|Abroad - details}
 					</td>
 				</tr>
 				<tr>
-					<td>24. Please describe how you will ensure that you comply with the General Data Protection Regulation (GDPR). [<a href="/research/ethics/dataprotection.html" target="_blank" title="[Link opens in a new window]">More info</a>]</td>
-					<td>{dataProtection}</td>
+					<td>24. Please describe how you will ensure that you comply with the General Data Protection Regulation (GDPR). [<a href=\"/research/ethics/dataprotection.html\" target=\"_blank\" title=\"[Link opens in a new window]\">More info</a>]</td>
+					<td>{dataProtection|mediumtext|Data protection}</td>
 				</tr>
 			</tbody>
 		</table>
-		';
+		";
 		
 		# Final confirmation
 		$html .= '
